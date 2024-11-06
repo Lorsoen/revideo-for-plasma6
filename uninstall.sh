7a67dd20f2943bb75bc564ff37b610f58c865a23
Lorenzo <fashim99@gmail.com>
2024-11-06 21:47:11 +0100

    Correct

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 #
-# 	Part of kde-service-menu-reimage Version 2.5
+# 	Part of kde-service-menu-revideo Version 0.2.0
 # 	Copyright (C) 2018-2019 Giuseppe Benigno <giuseppe.benigno(at)gmail.com>
+# 	Copyright (C) 2024 <fashim99.(at)gmail.com>
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
@@ -17,32 +18,39 @@
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# environment variables
+qtpaths_bin="${qtpaths_bin:-"qtpaths"}"
+user_install_prefix="${user_install_prefix:-"${HOME}/.local"}"
+
+# local variables
+required_vars=("bin_dir" "servicemenu_dir" "doc_dir")
+
 if [[ $EUID -eq 0 ]]; then
-    bin_dir="$(kf5-config --path exe | sed "s/.*://")"
-    desktop_dir="$(kf5-config --path services | sed "s/.*://")ServiceMenus/"
-    doc_dir="$(kf5-config --prefix)/share/doc/kde-service-menu-reimage/"
+    bin_dir="$(${qtpaths_bin} --install-prefix)/bin"
+    servicemenu_dir="$(${qtpaths_bin} --locate-dirs GenericDataLocation kio/servicemenus | sed 's/.*://')"
+    doc_dir="$(${qtpaths_bin} --install-prefix)/share/doc/kde-service-menu-revideo/"
     echo "Removing kde-service-menu-reimage system wide"
 else
-    bin_dir="$HOME/bin"
-    desktop_dir="$(kf5-config --path services | sed "s/:.*//")"
-    doc_dir=$HOME"/share/doc/kde-service-menu-reimage/"
+    bin_dir="${user_install_prefix}/bin"
+    servicemenu_dir="$(${qtpaths_bin} --locate-dirs GenericDataLocation kio/servicemenus | sed 's/:.*//')"
+    doc_dir="${user_install_prefix}/share/doc/kde-service-menu-revideo/"
     echo "Removing kde-service-menu-reimage locally"
 fi
 
 echo "removing ${bin_dir}reimage-kdialog"
 rm "${bin_dir}/reimage-kdialog"
 
-echo "removing ${desktop_dir}reimage-compress-resize.desktop"
-rm "${desktop_dir}reimage-compress-resize.desktop"
-echo "removing ${desktop_dir}reimage-convert-rotate.desktop"
-rm "${desktop_dir}reimage-convert-rotate.desktop"
-echo "removing ${desktop_dir}reimage-metadata.desktop"
-rm "${desktop_dir}reimage-metadata.desktop"
-echo "removing ${desktop_dir}reimage-tools.desktop"
-rm "${desktop_dir}reimage-tools.desktop"
+echo "removing ${servicemenu_dir}reimage-compress-resize.desktop"
+rm "${servicemenu_dir}reimage-compress-resize.desktop"
+echo "removing ${servicemenu_dir}reimage-convert-rotate.desktop"
+rm "${servicemenu_dir}reimage-convert-rotate.desktop"
+echo "removing ${servicemenu_dir}reimage-metadata.desktop"
+rm "${servicemenu_dir}reimage-metadata.desktop"
+echo "removing ${servicemenu_dir}reimage-tools.desktop"
+rm "${servicemenu_dir}reimage-tools.desktop"
 
 echo "removing ${doc_dir}"
 rm -rf "${doc_dir}"
 
 echo
-echo "kde-service-menu-reimage has been removed. Good bye."
+echo "kde-service-menu-revideo has been removed. Goodbye 😢"
